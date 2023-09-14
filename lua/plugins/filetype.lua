fischerling/vis

commit 8a420ecc4c1ed50111464ec66901bd983eaf2dbd

Resync the lexers with Scintillua
- Resync the lexers with Scintillua

- Update the lexer readme

- Update `zenburn` theme to fix some highlighting issues

- lexers: redirect print function to vis:info()

- Fix support for custom style names

- As per error message "lexer.delimited_range() is deprecated, use lexer.range()".

- Remove remaining `lexer.delimited_range()` call

- Set syntax to `nil` if the file type has no matching lexer

- Updated Go lexer for Go 1.18.

- lexers/dsv: convert to new lexer format

  (cherry picked from commit 9edbc3cd9ea1d7142b1305840432a3d2739e755a)

- lexers/gemini: disable legacy gemini lexer

  This reverts commit 468f9ee1b027a7ce98b1a249fa1af5888feeb989.

  It is in legacy format and of questionable quality. Ideally it
  should be contributed upstream from where it will eventually
  trickle down to us.

- lexers/git-rebase: convert to new lexer format

  (cherry picked from commit 4000a4cc9ac4a4c2869dfae772b977a82aee8d8c)

- lexers/strace: convert to new lexer format

  (cherry picked from commit e420451320d97eb164f5629c1bcfab0b595be29d)

- lexers/typescript: add new upstream lexer revision 28e2b60

  (cherry picked from commit 7326e6deecdaa75fa94ae9ebdb653f9f907b33f2)

- use `package.searchpath` instead of a local `searchpath` function

- Restore `filetype: support filetype detection via hashbang`

- Remove redundant comment

- Restore gemini lexer

lua/plugins/filetype.lua
@@ -164,6 +164,10 @@ vis.ftdetect.filetypes = {
 	gherkin = {
 		ext = { "%.feature$" },
 	},
+	['git-commit'] = {
+		ext = { "^COMMIT_EDITMSG$" },
+		cmd = { "set colorcolumn 72" },
+	},
 	['git-rebase'] = {
 		ext = { "git%-rebase%-todo" },
 	},
@@ -212,7 +216,10 @@ vis.ftdetect.filetypes = {
 		ext = { "%.bsh$", "%.java$" },
 	},
 	javascript = {
-		ext = { "%.cjs$", "%.js$", "%.jsfl$", "%.mjs$", "%.ts$", "%.jsx$", "%.tsx$" },
+		ext = { "%.cjs$", "%.js$", "%.jsfl$", "%.mjs$" },
+	},
+	jq = {
+		ext = { "%.jq$" },
 	},
 	json = {
 		ext = { "%.json$" },
@@ -435,6 +442,9 @@ vis.ftdetect.filetypes = {
 	toml = {
 		ext = { "%.toml$" },
 	},
+	typescript = {
+		ext = { "%.ts$" },
+	},
 	vala = {
 		ext = { "%.vala$" }
 	},
@@ -484,7 +494,13 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 		for _, cmd in pairs(filetype.cmd or {}) do
 			vis:command(cmd)
 		end
-		win:set_syntax(syntax)
+		local path = vis.lexers.property['lexer.lpeg.home']:gsub(';', '/?.lua;') .. '/?.lua'
+		local lexpath = package.searchpath('lexers/'..syntax, path)
+		if lexpath ~= nil then
+			win:set_syntax(syntax)
+		else
+			win:set_syntax(nil)
+		end
 	end
 
 	local path = win.file.name -- filepath

commit cf0da6a3a3c5c0a4dcc586fc949f9f4d48b80b37

remove filetype detection for removed lexer
Commit 8a420ecc4c1ed50111464ec66901bd983eaf2dbd resyncing the
lexers reintroduced detection for a deleted lexer.

Fixes: 8a420ecc4c1ed50111464ec66901bd983eaf2dbd

lua/plugins/filetype.lua
@@ -167,10 +167,6 @@ vis.ftdetect.filetypes = {
 	gherkin = {
 		ext = { "%.feature$" },
 	},
-	['git-commit'] = {
-		ext = { "^COMMIT_EDITMSG$" },
-		cmd = { "set colorcolumn 72" },
-	},
 	['git-rebase'] = {
 		ext = { "git%-rebase%-todo" },
 	},